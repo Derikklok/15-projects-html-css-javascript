e761f244947cd7b2ce21590e05885e6d12ee71bf
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-08-19 21:58:08 +0530

    second update

---
+++ expense-tracker/script.js
@@ -0,0 +1,40 @@
+const balanceEl = document.getElementById("balance");
+const incomeAmountEl = document.getElementById("income-amount");
+const expenseAmountEl = document.getElementById("expense-amount");
+const transactionListEl = document.getElementById("transaction-list");
+const transactionFormEl = document.getElementById("transaction-form");
+const descriptionEl = document.getElementById("description");
+const amountEl = document.getElementById("amount");
+
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+transactionFormEl.addEventListener("submit", addTransaction);
+
+function addTransaction(e) {
+    // Prevent from refreshing the page
+  e.preventDefault();
+
+  const description = descriptionEl.value.trim();
+  console.log(description);
+  const amount = parseFloat(amountEl.value);
+  console.log(amount);
+
+  transactions.push({
+    id:Date.now(),
+    description,
+    amount
+  })
+
+  localStorage.setItem("transactions",JSON.stringify(transactions));
+
+  updateTransactionList()
+  updateSummary()
+
+  transactionFormEl.reset();
+
+}
+
+
+function updateTransactionList(){
+    
+}
